769a2225b9275d9890a9ee3b9df274564653020b
dhsenior <daehwadaehwa744@gmail.com>
2026-06-19 16:11:27 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "본인의_API_KEY",
+  authDomain: "본인의_PROJECT_ID.firebaseapp.com",
+  projectId: "본인의_PROJECT_ID",
+  storageBucket: "본인의_PROJECT_ID.appspot.com",
+  messagingSenderId: "본인의_SENDER_ID",
+  appId: "본인의_APP_ID"
+};
